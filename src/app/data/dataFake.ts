6be0040383e0b7cb8044549f7cc9a02e3141666a
nicolas <nicolasvitoorino@gmail.com>
2023-10-20 11:38:26 -0300

    creating other pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photo":"aaa",
+    "title":"saiu novo filme",
+    "description": "aaaa"
+  },
+  {
+    "id":2,
+    "photo":"aaa",
+    "title":"saiu novo filme",
+    "description": "aaaa"
+  }
+]
